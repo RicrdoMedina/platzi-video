fd3ce47480ae9bac651d0e3b1d7244585c6d59f2
Ricardo Medina <rcrdmedina6@gmail.com>
2018-05-12 16:54:48 -0430

    Iniciando proyecto

---
+++ index.js
@@ -0,0 +1,8 @@
+import ReactDOM from 'react-dom'
+import React from 'react'
+
+const app = document.getElementById('app')
+
+const holaMundo = <h1>Hola Mu</h1>;
+
+ReactDOM.render(holaMundo, app);
---
+++ webpack.dev.config.js
@@ -0,0 +1,59 @@
+const path = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+  entry: {
+    'platzi-video': path.resolve(__dirname, 'index.js'),
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'js/[name].js'
+  },
+  devServer: {
+    port: 9000,
+    hot: true, // Para usar el HotModuleReplacement agregar el src que apunte al server <script src="http://localhost:8080/js/bundle.js"></script>
+    open: true
+  },
+  //devtool: 'eval-source-map',
+  module: {
+    rules: [
+      {
+        test: /\.(js)$/,
+        loader: 'eslint-loader',
+        enforce: 'pre',
+        include: [path.resolve(__dirname, './src')]
+      },
+      {
+        // test: que tipo de archivo quiero reconocer,
+        // use: que loader se va a encargar del archivo
+        test: /\.(js|jsx)$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['es2015', 'react', 'stage-2'],
+          }
+        },
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.(jpg|png|gif|svg)$/,
+        use: {
+          loader: 'url-loader',
+          options: {
+            limit: 1000000,
+            fallback: 'file-loader',
+            name: 'images/[name].[hash].[ext]',
+          }
+        }
+      },
+    ]
+  },
+  plugins: [
+    //Activar HotModuleReplacement
+    new webpack.HotModuleReplacementPlugin()
+  ]
+}
